339597d8527f60c41beebca05f096463de568ace
leonardoalmeida <leonardo.almeida@kobe.io>
2021-11-30 14:49:06 -0300

    ajuste usuario banco

--- auth/main.py
+++ auth/main.py
@@ -15,7 +15,7 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-app = create_app() # we initialize our flask app using the __init__.py function
+app = create_app()
 if __name__ == '__main__':
-    db.create_all(app=create_app()) # create the SQLite database
-    app.run(debug=True) # run the flask app on debug mode
+    db.create_all(app=create_app()) 
+    app.run(debug=True) 
